4b1f4cbe35921b932f4c8497a9bbb0a39857d2ce
Luan-LopS <luam10@hotmail.com.br>
2024-05-31 18:01:31 -0300

    criando chão

--- scrpts/main.js
+++ scrpts/main.js
@@ -1,7 +1,6 @@
 const canva = document.getElementById('gameBoard')
 const contexto = canva.getContext('2d')
 
-
 class Player{
     constructor(x,y){
         this.x = x;
@@ -10,20 +9,13 @@ class Player{
         this.height = 20;
         this.speed = 5;
         this.gravidade = 0;
-        this.jumPower = -10;
+        this.jumPower = -8;
         this.isJumping = false
     }
 
     draw(){
         contexto.fillStyle = 'blue';
         contexto.fillRect(this.x,this.y, this.width, this.height)
-    }
-
-    update(){
-        this.y += this.gravidade
-        if(this.isJumping){
-            this.gravidade += 0.5
-        }
     }
 
     jump(){
@@ -34,17 +26,48 @@ class Player{
     }
 }
 
-const player = new Player(50,50)
-
 class Box{
     constructor(x,y){
         this.x = x;
         this.y = y;
         this.height = 20;
         this.width = 20;
+    }
 
+    draw(){
+        contexto.fillStyle = 'blue';
+        contexto.fillRect(this.x,this.y, this.width, this.height)
     }
 }
+
+class Floor{
+    constructor(x, y){
+        this.x=x;
+        this.y=y;
+        this.height = 5;
+        this.width = 200;
+    }
+
+    draw(){
+        contexto.fillStyle = 'brown';
+        contexto.fillRect(this.x,this.y, this.width, this.height)
+    }
+
+
+}
+
+const player = new Player(5,125)
+const floor = new Floor(0,145)
+const boxLife = new Box( )
+
+
+update(){
+    player.y += player.gravidade
+    if(player.isJumping){
+        player.gravidade += 0.3
+    }
+}
+
 
 
 document.addEventListener('keydown',function(e){
@@ -58,14 +81,9 @@ document.addEventListener('keydown',function(e){
 
 })
 
-// document.addEventListener('keyup', function(e){
-//     if(e.keyCode === 39 &&  e.keyCode === 37 && e.shiftKey === 32){
-
-//     }
-// })
-
 function gameLoop(){
     contexto.clearRect(0,0,canva.width,canva.height)
+    floor.draw()
     player.draw()
     player.update()
 
@@ -77,3 +95,50 @@ document.getElementById('playButton').addEventListener('click', function(){
     console.log('Play')
     document.getElementsByTagName('canvas')[0].style.display = 'block'
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Player {
+    // ... (seu código existente)
+
+    update() {
+        // Verifica se o jogador está no chão
+        if (this.y >= floor.y - this.height) {
+            this.isJumping = false; // O jogador não está pulando
+            this.y = floor.y - this.height; // Mantém o jogador no chão
+        } else {
+            // Aplica a gravidade normalmente
+            this.y += this.gravidade;
+            this.gravidade += 0.3;
+        }
+    }
+
+    // ... (seu código existente)
+}
